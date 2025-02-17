599c98b41ccbb96cd015380a466c2bd94498e9c1
es-osu <stephens.872@osu.edu>
2025-02-17 16:05:01 -0500

    Adding all routes & files neccessary to show the correct navbar to each user based on user role

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,18 @@ Rails.application.routes.draw do
   # Signup route ideas - not implemented
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+
+  get 'dashboard/landlord', to: 'dashboard#landlord', as: 'landlord_dashboard'
+  get 'dashboard/tenant', to: 'dashboard#tenant', as: 'tenant_dashboard'
+  get 'dashboard/admin', to: 'dashboard#admin', as: 'admin_dashboard'
+  get 'dashboard/mediator', to: 'dashboard#mediator', as: 'mediator_dashboard'
+
+  resources :messages, only: [:index, :show, :create, :destroy]
+  resources :documents, only: [:index, :show, :create, :destroy]
+  resources :resources, only: [:index]
+  resources :mediations, only: [:index, :show, :create, :destroy]
+  resources :accounts, only: [:index, :show, :create, :destroy]
+  resource :system_data, only: [:show]
+  resource :account, only: [:show, :edit, :update]
+
 end
