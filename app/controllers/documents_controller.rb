36c90c28c554d5b7430697e74189a7c8e6f66a34
es-osu <stephens.872@osu.edu>
2025-02-17 17:02:51 -0500

    generated documents controller & linked it to navbar for tenants & landlords

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,27 @@
+class DocumentsController < ApplicationController
+  before_action :require_login
+  before_action :set_user
+
+  def index
+    case @user.Role
+    when "Tenant"
+      render "documents/tenant_index"
+    when "Landlord"
+      render "documents/landlord_index"
+    else
+      render plain: "Access Denied", status: :forbidden
+    end
+  end
+
+  private
+  
+  def require_login
+    unless session[:user_id]
+      redirect_to login_path, alert: "You must be logged in to access the dashboard."
+    end
+  end
+
+  def set_user
+    @user = User.find(session[:user_id])
+  end
+end
