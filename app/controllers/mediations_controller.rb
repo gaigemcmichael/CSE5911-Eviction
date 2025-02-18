aa3602af6c093d00999ffb9b18d5c68d310070a7
es-osu <stephens.872@osu.edu>
2025-02-18 16:17:17 -0500

    use our standard for login requirements

--- app/controllers/mediations_controller.rb
+++ app/controllers/mediations_controller.rb
@@ -1,10 +1,16 @@
 class MediationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :require_login
   before_action :require_tenant_or_landlord_role, only: [:index, :create, :respond]
 
-  
+
 
   private 
+
+  def require_login
+    unless session[:user_id]
+      redirect_to login_path, alert: "You must be logged in to access the dashboard."
+    end
+  end
 
   def require_tenant_or_landlord_role
     unless ["Tenant", "Landlord"].include?(current_user.Role)
