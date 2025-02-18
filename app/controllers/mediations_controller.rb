89b0571e1edda2dbd75713155a0521d414101370
es-osu <stephens.872@osu.edu>
2025-02-18 16:42:18 -0500

    adding logic to show landlord/tenant create mediation feature unless mediation is already ongoing, still need to handle creating the mediation itself.

--- app/controllers/mediations_controller.rb
+++ app/controllers/mediations_controller.rb
@@ -1,14 +1,33 @@
 class MediationsController < ApplicationController
   before_action :require_login
   before_action :require_tenant_or_landlord_role, only: [:index, :create, :respond]
+  before_action :set_user
 
+  def index
+    if @user.Role == "Tenant"
+      @mediation = PrimaryMessageGroups.find_by(TenantID: @user.UserID) # Find the single mediation the tenant is involved in.
+    elsif @user.Role == "Landlord"
+      @mediation = PrimaryMessageGroups.where(LandlordID: @user.UserID) # Find the possibly multiple mediations the landlord is involve in.
+    end 
 
+    @show_mediation_view = @mediation.present? # this is used by the view to determine if we show current mediations or if we need to show create mediations
+  end
+
+  def create
+  end
+
+  def respond
+  end
 
   private 
 
+  def set_user
+    @user = User.find(session[:user_id])
+  end
+
   def require_login
     unless session[:user_id]
-      redirect_to login_path, alert: "You must be logged in to access the dashboard."
+      redirect_to login_path, alert: "You must be logged in to access the mediations."
     end
   end
 
