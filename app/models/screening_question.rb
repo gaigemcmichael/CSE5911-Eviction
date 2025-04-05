a2edd9927088db4634e054f30f0b84bed685ce9b
es-osu <stephens.872@osu.edu>
2025-04-05 16:37:28 -0400

    Fixing the screening questions so they work properly and assert required answers

--- app/models/screening_question.rb
+++ app/models/screening_question.rb
@@ -1,3 +1,8 @@
 class ScreeningQuestion < ApplicationRecord
     self.table_name = "ScreeningQuestions"
+
+    # Ensure users fill out alll required fields
+    validates :InterpreterNeeded, :DisabilityAccommodation, :ConflictOfInterest,
+          :SpeakOnOwnBehalf, :NeedToConsult, :Unsafe,
+          inclusion: { in: [true, false], message: "must be selected" }
 end
