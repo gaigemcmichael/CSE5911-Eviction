2baabb39ad0b01dbe6a89d77df226bb5804de177
R4tthew <mattDippy02@gmail.com>
2025-04-06 23:05:14 -0400

    Migrations for adding the intake questions. Intake questions complete as well. Linkage/conditional rendering should work.

---
+++ app/models/intake_question.rb
@@ -0,0 +1,23 @@
+class IntakeQuestion < ApplicationRecord
+    self.table_name = "IntakeQuestions"
+  
+    belongs_to :user, foreign_key: "UserID"
+  
+    validates :Reason, inclusion: {
+      in: [
+        "Failure to Pay Rent",
+        "Violation of Lease Terms",
+        "Damage to Property",
+        "Illegal Activity",
+        "Nuisance or Disturbance",
+        "Expiration of Lease",
+        "Unknown"
+      ]
+    }
+  
+    validates :BestOption, inclusion: {
+      in: ["Pay Missed Rent", "Move Out"]
+    }
+  
+    validates :Section8, :MoneyOwed, :TotalCostOrMonthly, presence: true
+  end
