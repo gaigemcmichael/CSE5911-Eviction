d599e618b789d311869ddde29c9db13085909d8b
R4tthew <mattDippy02@gmail.com>
2025-04-13 02:42:17 -0400

    Summary works for signed files from Primary Message Groups. Still working on the side message groups.

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -1,3 +1,7 @@
 class FileAttachment < ApplicationRecord
     self.table_name = "FileAttachments"
+
+    belongs_to :file_draft, foreign_key: :FileID
+    belongs_to :message, foreign_key: :MessageID
+
 end
--- app/models/file_draft.rb
+++ app/models/file_draft.rb
@@ -1,3 +1,6 @@
 class FileDraft < ApplicationRecord
     self.table_name = "FileDrafts"
+
+    has_many :file_attachments, foreign_key: :FileID
+
 end
