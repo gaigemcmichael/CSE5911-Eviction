1745aed6e951ad65cb05222c8e9bc8ddd76d214b
tdp3 <donnelly.208@osu.edu>
2025-10-30 12:53:52 -0400

    landlord mailer test

--- test/mailers/landlord_mailer_test.rb
+++ test/mailers/landlord_mailer_test.rb
@@ -1,23 +1,11 @@
 require "test_helper"
-require "cgi"
 
 class LandlordMailerTest < ActionMailer::TestCase
   test "invitation_email" do
-    previous_sender = ENV["GMAIL_USERNAME"]
-    ENV["GMAIL_USERNAME"] = "notifications@example.com"
-
-    inviter = users(:tenant1)
-    recipient_email = "new-landlord@example.com"
-
-    mail = LandlordMailer.invitation_email(recipient_email, inviter)
-
-    assert_equal [ recipient_email ], mail.to
-    assert_equal [ "notifications@example.com" ], mail.from
+    invited_by_user = users(:one)
+    mail = LandlordMailer.invitation_email("to@example.org", invited_by_user)
     assert_equal "You are invited to join the Mediation Platform", mail.subject
-  assert_includes mail.body.encoded, inviter.FName
-  escaped_query = CGI.escapeHTML("signup?role=Landlord&email=#{CGI.escape(recipient_email)}")
-  assert_includes mail.body.encoded, escaped_query
-  ensure
-    ENV["GMAIL_USERNAME"] = previous_sender
+    assert_equal [ "to@example.org" ], mail.to
+    assert_match invited_by_user.FName, mail.body.encoded
   end
 end
