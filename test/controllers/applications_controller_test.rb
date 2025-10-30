b8f3923916a5d6ad8d9fcee734b26bc4eb0fbfee
tdp3 <donnelly.208@osu.edu>
2025-10-30 12:55:40 -0400

    application controller test commented out, its all scaffolding befoer and after

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
-class ApplicationsControllerTest < ActionDispatch::IntegrationTest
-  test "applications controller not implemented" do
-    skip "Applications controller is not implemented in the application."
-  end
-end
+# Just a scaffold, will need filled in
+# class ApplicationsControllerTest < ActionDispatch::IntegrationTest
+#   test "should get index" do
+#     skip "ApplicationsController doesn't exist - this is scaffold-generated test"
+#   end
+
+#   test "should get new" do
+#     skip "ApplicationsController doesn't exist - this is scaffold-generated test"
+#   end
+
+#   test "should create application" do
+#     skip "ApplicationsController doesn't exist - this is scaffold-generated test"
+#   end
+
+#   test "should show application" do
+#     skip "ApplicationsController doesn't exist - this is scaffold-generated test"
+#   end
+
+#   test "should get edit" do
+#     skip "ApplicationsController doesn't exist - this is scaffold-generated test"
+#   end
+
+#   test "should update application" do
+#     skip "ApplicationsController doesn't exist - this is scaffold-generated test"
+#   end
+
+#   test "should destroy application" do
+#     skip "ApplicationsController doesn't exist - this is scaffold-generated test"
+#   end
+# end
