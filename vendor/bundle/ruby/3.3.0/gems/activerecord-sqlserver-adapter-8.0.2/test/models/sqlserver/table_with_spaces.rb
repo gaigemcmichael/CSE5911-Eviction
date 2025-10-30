ae9888cbcb15fba79a87bd0736599bce9ea0a291
Gaige McMichael <gaigemcmichael@gmail.com>
2025-10-30 18:18:17 -0400

    Remove vendored bundle and per-env credentials from tracking; update .gitignore

--- vendor/bundle/ruby/3.3.0/gems/activerecord-sqlserver-adapter-8.0.2/test/models/sqlserver/table_with_spaces.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class TableWithSpaces < ActiveRecord::Base
-  self.table_name = "A Table With Spaces"
-end
--- vendor/bundle/ruby/3.3.0/gems/font-awesome-sass-6.4.2/lib/font_awesome/sass/version.rb
+++
@@ -1,5 +0,0 @@
-module FontAwesome
-  module Sass
-    VERSION = "6.4.2".freeze
-  end
-end
